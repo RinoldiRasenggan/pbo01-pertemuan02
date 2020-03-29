858fe392292a36826d3a3a2f53d766e92da6aebc
RinoldiRasenggan <muhammadrinoldi11@gmail.com>
2020-03-30 00:04:27 +0800

    Memanggil method dari class di package lain

--- Main.java
+++ Main.java
@@ -1,14 +1,8 @@
 import koneksi.Database;
-import model.Dosen;
-import model.Mahasiswa;
-import model.MataKuliah;
 
 public class main{
  public static void main(String [] args){
   System.out.println("ini program main");
   Database.hubungkan();
-  Dosen.tampilkanInfo();
-Mahasiswa.tampilkanInfo();
-MataKuliah.tampilkanInfo();
  }
 }
